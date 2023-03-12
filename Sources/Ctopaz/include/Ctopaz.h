2f3f1b16446123af73d0cf5de352efc4a59593fe
Tanner Silva <t@tannersilva.com>
2023-03-12 11:32:32 -0500

    fuck it lets include all of them?

--- Sources/Ctopaz/include/Ctopaz.h
+++ Sources/Ctopaz/include/Ctopaz.h
@@ -1,11 +1,37 @@
 #ifndef  CTOPAZ_H
 #define  CTOPAZ_H
 
-#include "hex.h"
 #include <stdio.h>
-#include "bolt11.h"
+#include "alignof.h"
 #include "amount.h"
+#include "array_size.h"
+#include "bech32.h"
+#include "bech32_util.h"
+#include "bolt11.h"
+#include "build_assert.h"
+#include "check_type.h"
+#include "compiler.h"
+#include "config.h"
+#include "container_of.h"
+#include "cppmagic.h"
+#include "endian.h"
 #include "hash_u5.h"
+#include "hex.h"
+#include "likely.h"
+#include "list.h"
+#include "mem.h"
+#include "node_id.h"
+#include "overflows.h"
+#include "sha256.h"
+#include "short_types.h"
+#include "str.h"
+#include "str_debug.h"
+#include "structeq.h"
+#include "take.h"
+#include "tal.h"
+#include "talstr.h"
+#include "typesafe_cb.h"
+#include "utf8.h"
 
 #define  MAX_BLOCKS 1024
 
